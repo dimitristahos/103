df66275bc6002ee2004dc30dd11cbb2db89155a6
dtahos <dimitristahos@gmal.com>
2024-11-05 00:46:54 +0200

    Nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+
   plugins: [
     {
       src: "~/plugins/pocketbase.client",
       mode: "client",
     },
   ],
+
   devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
 });
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,32 @@
   const { $pb } = useNuxtApp();
   const resultList = ref([]);
 
+  const items = computed(() => {
+    return resultList.value.items?.map((item) => {
+      return {
+        label: item.Title,
+        images: item.expand?.Gallery?.map((image) => {
+          return $pb.files.getUrl(image, image.field, { thumb: "100x250" });
+        }),
+        ...item,
+      };
+    });
+  });
+
   onMounted(async () => {
-    resultList.value = await $pb.collection("posts").getList(1, 50);
+    resultList.value = await $pb.collection("posts").getList(1, 50, { expand: "Gallery" });
   });
 </script>
 
 <template>
-  <div>
-    <ul>
-      <li v-for="result in resultList.items" :key="result.id">
-        {{ result.Title }}
-      </li>
-    </ul>
-  </div>
+  <UContainer class="mt-8">
+    <UAccordion color="primary" variant="soft" size="sm" :items="items">
+      <template #item="{ item: { images, Description } }">
+        <div v-html="Description"></div>
+        <UCarousel v-slot="{ item }" :items="images">
+          <img :src="item" width="300" height="400" draggable="false" />
+        </UCarousel>
+      </template>
+    </UAccordion>
+  </UContainer>
 </template>
